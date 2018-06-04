e1d5a693ce1d9ee34ba87c18b478b41804ad4af2
mkht <mkht@users.noreply.github.com>
2018-06-04 13:25:46 +0900

    Returns own version #17 (#21)
    * pspm version
    * pspm -Version
    * pspm -v

--- functions/pspm.ps1
+++ functions/pspm.ps1
@@ -1,14 +1,14 @@
 function pspm {
-    [CmdletBinding(DefaultParameterSetName = 'Json')]
+    [CmdletBinding(DefaultParameterSetName = 'Install')]
     param
     (
         # Parameter help description
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Position = 0)]
         [string]
-        $Command = 'install',
+        $Command = 'version',
 
         # Parameter help description
-        [Parameter(position = 1, ParameterSetName = 'ModuleName')]
+        [Parameter(position = 1)]
         [ValidateNotNullOrEmpty()]
         [string]
         $Name,
@@ -29,7 +29,12 @@ function pspm {
         $Save,
 
         [Parameter()]
-        [switch]$Clean
+        [switch]$Clean,
+
+        [Parameter(ParameterSetName = 'Version')]
+        [alias('v')]
+        [switch]
+        $Version
     )
 
     #region Initialize
@@ -39,6 +44,13 @@ function pspm {
     $script:UserPSModulePath = Join-Path ([Environment]::GetFolderPath("MyDocuments")) 'WindowsPowerShell\Modules'
     $script:GlobalPSModulePath = Join-Path $env:ProgramFiles 'WindowsPowerShell\Modules'
     #endregion
+
+    # Get version of myself
+    if (($Command -eq 'version') -or ($PSCmdlet.ParameterSetName -eq 'Version')) {
+        $owmInfo = Import-PowerShellDataFile -LiteralPath (Join-Path -Path $script:ModuleRoot -ChildPath 'pspm.psd1')
+        [string]($owmInfo.ModuleVersion)
+        return
+    }
 
     #region Scope parameter
     if ($Global) {
@@ -74,13 +86,14 @@ function pspm {
         Get-ChildItem -Path $ModuleDir -Directory | Remove-Item -Recurse -Force
     }
 
-    if ($PSCmdlet.ParameterSetName -eq 'ModuleName') {
+    # Install from Name
+    if (($PSCmdlet.ParameterSetName -eq 'Install') -and (-not [String]::IsNullOrEmpty($Name))) {
         try {
-            $targetModule = getModule -Version $Name -Path $ModuleDir -ErrorAction Stop
+            $local:targetModule = getModule -Version $Name -Path $ModuleDir -ErrorAction Stop
 
-            if ($targetModule) {
-                Write-Host ('{0}@{1}: Importing module.' -f $targetModule.Name, $targetModule.ModuleVersion)
-                Import-Module (Join-path $ModuleDir $targetModule.Name) -Force -Global -ErrorAction Stop
+            if ($local:targetModule) {
+                Write-Host ('{0}@{1}: Importing module.' -f $local:targetModule.Name, $local:targetModule.ModuleVersion)
+                Import-Module (Join-path $ModuleDir $local:targetModule.Name) -Force -Global -ErrorAction Stop
 
                 if ($Save) {
                     if (Test-Path (Join-path $CurrentDir '\package.json')) {
@@ -95,7 +108,7 @@ function pspm {
                         }
                     }
 
-                    $PackageJson.dependencies | Add-Member -NotePropertyName $targetModule.Name -NotePropertyValue ([string]$targetModule.ModuleVersion) -Force
+                    $PackageJson.dependencies | Add-Member -NotePropertyName $local:targetModule.Name -NotePropertyValue ([string]$local:targetModule.ModuleVersion) -Force
                     $PackageJson | ConvertTo-Json | Format-Json | Out-File -FilePath (Join-path $CurrentDir '\package.json') -Force -Encoding utf8
                 }
             }
@@ -104,25 +117,26 @@ function pspm {
             Write-Error ('{0}: {1}' -f $Name, $_.Exception.Message)
         }
     }
-    elseif ($PSCmdlet.ParameterSetName -eq 'Json') {
+    # Install from package.json
+    elseif ($PSCmdlet.ParameterSetName -eq 'Install') {
         if (Test-Path (Join-path $CurrentDir '\package.json')) {
             $PackageJson = Get-Content -Path (Join-path $CurrentDir '\package.json') -Raw | ConvertFrom-Json
             
             $PackageJson.dependencies | Get-Member -MemberType NoteProperty | `
                 ForEach-Object {
-                $moduleName = $_.Name
-                $version = $PackageJson.dependencies.($_.Name)
+                $local:moduleName = $_.Name
+                $local:moduleVersion = $PackageJson.dependencies.($_.Name)
 
                 try {
-                    $targetModule = getModule -Name $moduleName -Version $version -Path $ModuleDir -ErrorAction Stop
+                    $local:targetModule = getModule -Name $local:moduleName -Version $local:moduleVersion -Path $ModuleDir -ErrorAction Stop
                 
-                    if ($targetModule) {
-                        Write-Host ('{0}@{1}: Importing module.' -f $targetModule.Name, $targetModule.ModuleVersion)
-                        Import-Module (Join-path $ModuleDir $targetModule.Name) -Force -Global -ErrorAction Stop
+                    if ($local:targetModule) {
+                        Write-Host ('{0}@{1}: Importing module.' -f $local:targetModule.Name, $local:targetModule.ModuleVersion)
+                        Import-Module (Join-path $ModuleDir $local:targetModule.Name) -Force -Global -ErrorAction Stop
                     }
                 }
                 catch {
-                    Write-Error ('{0}: {1}' -f $moduleName, $_.Exception.Message)
+                    Write-Error ('{0}: {1}' -f $local:moduleName, $_.Exception.Message)
                 }
             }
         }
